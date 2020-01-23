5260b744c6d92bbcbac3078e564d43a14ec9523e
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-23 22:26:09 +0100

    route file configured

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/auth/redirect/{provider}', 'SocialController@redirect');
+Route::get('/callback/{provider}', 'SocialController@callback');
